c1c15802f469bbbfe3908cd5e6cbd351c758c700
John Dykes <98johndykes@gmail.com>
2024-02-10 23:59:54 -0500

    less intense colors for social icons in footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ComponentProps, FunctionComponent } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMedium, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import jwmdykesFaviconURL from '../assets/jwmdykes-favicon.ico';
 
 interface FooterProps extends ComponentProps<'footer'> {}
@@ -15,28 +15,30 @@ const Footer: FunctionComponent<FooterProps> = () => {
       </ul>
       <div className='flex flex-row gap-5 items-center'>
         <span>Check me out at</span>
-        <ul className='flex items-center gap-3 text-themeBlack'>
-          <li className='hover:text-teal-400'>
-            <a href='https://github.com/jwmdykes'>
-              <FontAwesomeIcon icon={faMedium} size='xl' />
-            </a>
-          </li>
-          <li className='hover:text-teal-400'>
+
+        <ul className='flex items-center gap-3'>
+          <li className='hover:text-themeBlack'>
             <a href='https://github.com/jwmdykes'>
               <FontAwesomeIcon icon={faGithub} size='xl'></FontAwesomeIcon>
             </a>
           </li>
-          <li className='hover:text-teal-400'>
+          <li className='hover:text-themeBlack'>
             <a href='mailto:98johndykes@gmail.com'>
               <FontAwesomeIcon icon={faEnvelope} size='xl'></FontAwesomeIcon>
             </a>
           </li>
+          <li className='hover:text-themeBlack'>
+            <a href='https://github.com/jwmdykes'>
+              <FontAwesomeIcon icon={faMedium} size='xl' />
+            </a>
+          </li>
+
           <ul>
             <a href='https://jwmdykes.ca'>
               <img
                 src={jwmdykesFaviconURL}
                 alt='jwmdykes.ca favicon'
-                className='max-h-6 hover:brightness-125'
+                className='max-h-5 brightness-100 hover:brightness-125 saturate-0 hover:saturate-100'
               />
             </a>
           </ul>

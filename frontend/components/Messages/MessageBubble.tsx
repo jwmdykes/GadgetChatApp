2895aa0f4d8bb3a7cd168724cf1505093ca31378
John Dykes <98johndykes@gmail.com>
2024-02-12 23:38:42 -0500

    different bubble style for current user

--- frontend/components/Messages/MessageBubble.tsx
+++ frontend/components/Messages/MessageBubble.tsx
@@ -13,12 +13,13 @@ const MessageBubble: FunctionComponent<MessageBubbleProps> = ({
   console.log('IN MESSAGEBUBBLE');
   console.log(message);
   console.log(user);
-  const bubbleColor = message.user === user.id ? 'bg-blue-100' : 'bg-green-100';
+  const bubbleColor =
+    message.user === user.id
+      ? 'bg-blue-100 ml-auto rounded-tr-2xl rounded-l-2xl'
+      : 'bg-green-100 rounded-tl-2xl rounded-r-2xl';
 
   return (
-    <section
-      className={`p-4 rounded-r-2xl rounded-tl-2xl w-fit max-w-2xl h-fit prose ${bubbleColor}`}
-    >
+    <section className={`p-4 w-fit max-w-2xl h-fit prose ${bubbleColor}`}>
       {message.content}
     </section>
   );

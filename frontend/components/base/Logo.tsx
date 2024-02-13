b4c28854ac5cc4d2145640db3f50da0f6eaf6c80
John Dykes <98johndykes@gmail.com>
2024-02-12 22:53:58 -0500

    Modify logo slightly

--- frontend/components/base/Logo.tsx
+++ frontend/components/base/Logo.tsx
@@ -1,19 +1,20 @@
 import { FunctionComponent } from 'react';
-import React from 'react';
 import logoUrl from '../../assets/logo-notext.png';
 
 interface LogoProps {}
 
 const Logo: FunctionComponent<LogoProps> = () => (
-  <a href='/' rel='noreferrer' className='max-h-fit'>
-    <div className='flex items-center max-h-full h-5/6'>
-      <img
-        src={logoUrl}
-        alt='QuickQuack Logo'
-        className='h-full object-scale-down -mt-1'
-      />
-      <span className='font-bold text-xl tracking-tight'>QuickQuack</span>
-    </div>
+  <a
+    href='/'
+    rel='noreferrer'
+    className='max-h-fit flex justify-center items-center'
+  >
+    <img
+      src={logoUrl}
+      alt='QuickQuack Logo'
+      className='h-14 object-scale-down -mt-1'
+    />
+    <span className='font-bold text-xl tracking-tight -ml-1'>QuickQuack</span>
   </a>
 );
 
